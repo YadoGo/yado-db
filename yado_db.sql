e8421f67b5985b30474fa0737e5f6b792c128bcc
Ernest Magriñá <ernest8081@gmail.com>
2023-08-14 20:00:21 +0200

    refactor: ♻️ Change the user, and implemented the not null

--- yado_db.sql
+++ yado_db.sql
@@ -1,53 +1,53 @@
-CREATE DATABASE IF NOT EXISTS yado_bd;
+CREATE DATABASE IF NOT EXISTS yado_db;
 
-USE yado_bd;
+USE yado_db;
 
 CREATE TABLE roles (
     id INT PRIMARY KEY,
-    name VARCHAR(50),
-    description VARCHAR(100)
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE users (
     uuid VARCHAR(36) PRIMARY KEY,
-    first_name VARCHAR(50),
-    last_name VARCHAR(100),
-    username VARCHAR(100),
-    email VARCHAR(100),
-    password VARCHAR(50),
-    gender CHAR,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    gender CHAR NULL,
+    image_profile VARBINARY(8000) NULL,
     role_id INT DEFAULT 1,
     FOREIGN KEY (role_id) REFERENCES roles(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE countries (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50)
+    name VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE states (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(200),
+    name VARCHAR(200) NOT NULL,
     country_id INT,
     FOREIGN KEY (country_id) REFERENCES countries(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE populations (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
+    name VARCHAR(50) NOT NULL,
     state_id INT,
     FOREIGN KEY (state_id) REFERENCES states(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE hotels (
     uuid VARCHAR(36) PRIMARY KEY,
-    name VARCHAR(50),
-    description VARCHAR(1500),
-    stars INT,
-    address VARCHAR(50),
-    latitude FLOAT,
-    longitude FLOAT,
-    num_visited INT DEFAULT 0,
+    name VARCHAR(50) NOT NULL,
+    description VARCHAR(1500) NOT NULL,
+    stars INT NOT NULL, 
+    address VARCHAR(50) NOT NULL,
+    latitude FLOAT NOT NULL,
+    longitude FLOAT NOT NULL,
+    num_visited INT DEFAULT 0 NULL,
     population_id INT,
     FOREIGN KEY (population_id) REFERENCES populations(id) ON UPDATE CASCADE ON DELETE CASCADE
 );
@@ -70,9 +70,9 @@ CREATE TABLE favorites (
 
 CREATE TABLE reviews (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    qualification FLOAT,
-    positive_comment VARCHAR(200),
-    negative_comment VARCHAR(200),
+    qualification FLOAT NOT NULL,
+    positive_comment VARCHAR(200) NOT NULL,
+    negative_comment VARCHAR(200) NULL,
     user_id VARCHAR(40),
     hotel_uuid VARCHAR(40),
     FOREIGN KEY (user_id) REFERENCES users(uuid) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -81,23 +81,23 @@ CREATE TABLE reviews (
 
 CREATE TABLE images (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    url VARCHAR(200),
-    description VARCHAR(200),
+    image VARCHAR(200) NOT NULL,
+    description VARCHAR(200) NOT NULL,
     hotel_uuid VARCHAR(40),
     FOREIGN KEY (hotel_uuid) REFERENCES hotels(uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE companies (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50),
-    url VARCHAR(200),
-    url_logo VARCHAR(200)
+    name VARCHAR(50) NOT NULL,
+    url VARCHAR(200) NOT NULL,
+    url_logo VARCHAR(200) NOT NULL
 );
 
 CREATE TABLE sites (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    url_origin VARCHAR(200),
-    nightly_price FLOAT,
+    url_origin VARCHAR(200) NOT NULL,
+    nightly_price FLOAT NOT NULL,
     hotel_uuid VARCHAR(40),
     company_id INT,
     FOREIGN KEY (hotel_uuid) REFERENCES hotels(uuid) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -106,55 +106,55 @@ CREATE TABLE sites (
 
 CREATE TABLE parameters (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    beach_distance FLOAT,
-    center_distance FLOAT,
-    bicycle_rental BOOLEAN,
-    solarium BOOLEAN,
-    golf_course BOOLEAN,
-    massage BOOLEAN,
-    fitness_centre BOOLEAN,
-    free_cancellation BOOLEAN,
-    self_catering BOOLEAN,
-    breakfast_included BOOLEAN,
-    breakfast_dinner_included BOOLEAN,
-    twin_beds BOOLEAN,
-    double_bed BOOLEAN,
-    large_double_bed BOOLEAN,
-    extra_large_double_bed BOOLEAN,
     review_score FLOAT,
-    non_smoking_rooms BOOLEAN,
-    parking BOOLEAN,
-    very_good_wifi BOOLEAN,
-    family_rooms BOOLEAN,
-    swimming_pool BOOLEAN,
-    room_service BOOLEAN,
-    pets_allowed BOOLEAN,
-    facilites_disabled_guest BOOLEAN,
-    spa_wellness_centre BOOLEAN,
-    aeroport_schuttle BOOLEAN,
-    restaurant BOOLEAN,
-    reception_always_open BOOLEAN,
-    kitchen BOOLEAN,
-    private_bathroom BOOLEAN,
-    air_conditioning BOOLEAN,
-    laptop_friendly_workspace BOOLEAN,
-    bath BOOLEAN,
-    private_pool BOOLEAN,
-    terrace BOOLEAN,
-    balcony BOOLEAN,
-    blat_screen_tv BOOLEAN,
-    washing_machine BOOLEAN,
-    patio BOOLEAN,
-    soundproofing BOOLEAN,
-    view_hotel BOOLEAN,
-    sea_view BOOLEAN,
-    wheelchair_accessible BOOLEAN,
-    toilet_grab_rails BOOLEAN,
-    higher_level_toilet BOOLEAN,
-    lower_bathroom_sink BOOLEAN,
-    emergency_cord_bathroom BOOLEAN,
-    visual_aids_braille BOOLEAN,
-    visual_aids_tactile_signs BOOLEAN,
+    beach_distance FLOAT DEFAULT 0 NOT NULL,
+    center_distance FLOAT DEFAULT 0 NOT NULL,
+    bicycle_rental BOOLEAN DEFAULT 0 NOT NULL,
+    solarium BOOLEAN DEFAULT 0 NOT NULL,
+    golf_course BOOLEAN DEFAULT 0 NOT NULL,
+    massage BOOLEAN DEFAULT 0 NOT NULL,
+    fitness_centre BOOLEAN DEFAULT 0 NOT NULL,
+    free_cancellation BOOLEAN DEFAULT 0 NOT NULL,
+    self_catering BOOLEAN DEFAULT 0 NOT NULL,
+    breakfast_included BOOLEAN DEFAULT 0 NOT NULL,
+    breakfast_dinner_included BOOLEAN DEFAULT 0 NOT NULL,
+    twin_beds BOOLEAN DEFAULT 0 NOT NULL,
+    double_bed BOOLEAN DEFAULT 0 NOT NULL,
+    large_double_bed BOOLEAN DEFAULT 0 NOT NULL,
+    extra_large_double_bed BOOLEAN DEFAULT 0 NOT NULL,
+    non_smoking_rooms BOOLEAN DEFAULT 0 NOT NULL,
+    parking BOOLEAN DEFAULT 0 NOT NULL,
+    very_good_wifi BOOLEAN DEFAULT 0 NOT NULL,
+    family_rooms BOOLEAN DEFAULT 0 NOT NULL,
+    swimming_pool BOOLEAN DEFAULT 0 NOT NULL,
+    room_service BOOLEAN DEFAULT 0 NOT NULL,
+    pets_allowed BOOLEAN DEFAULT 0 NOT NULL,
+    facilites_disabled_guest BOOLEAN DEFAULT 0 NOT NULL,
+    spa_wellness_centre BOOLEAN DEFAULT 0 NOT NULL,
+    aeroport_schuttle BOOLEAN DEFAULT 0 NOT NULL,
+    restaurant BOOLEAN DEFAULT 0 NOT NULL,
+    reception_always_open BOOLEAN DEFAULT 0 NOT NULL,
+    kitchen BOOLEAN DEFAULT 0 NOT NULL,
+    private_bathroom BOOLEAN DEFAULT 0 NOT NULL,
+    air_conditioning BOOLEAN DEFAULT 0 NOT NULL,
+    laptop_friendly_workspace BOOLEAN DEFAULT 0 NOT NULL,
+    bath BOOLEAN DEFAULT 0 NOT NULL,
+    private_pool BOOLEAN DEFAULT 0 NOT NULL,
+    terrace BOOLEAN DEFAULT 0 NOT NULL,
+    balcony BOOLEAN DEFAULT 0 NOT NULL,
+    blat_screen_tv BOOLEAN DEFAULT 0 NOT NULL,
+    washing_machine BOOLEAN DEFAULT 0 NOT NULL,
+    patio BOOLEAN DEFAULT 0 NOT NULL,
+    soundproofing BOOLEAN DEFAULT 0 NOT NULL,
+    view_hotel BOOLEAN DEFAULT 0 NOT NULL,
+    sea_view BOOLEAN DEFAULT 0 NOT NULL,
+    wheelchair_accessible BOOLEAN DEFAULT 0 NOT NULL,
+    toilet_grab_rails BOOLEAN DEFAULT 0 NOT NULL,
+    higher_level_toilet BOOLEAN DEFAULT 0 NOT NULL,
+    lower_bathroom_sink BOOLEAN DEFAULT 0 NOT NULL,
+    emergency_cord_bathroom BOOLEAN DEFAULT 0 NOT NULL,
+    visual_aids_braille BOOLEAN DEFAULT 0 NOT NULL,
+    visual_aids_tactile_signs BOOLEAN DEFAULT 0 NOT NULL,
     hotel_uuid VARCHAR(40),
     FOREIGN KEY (hotel_uuid) REFERENCES hotels(uuid) ON UPDATE CASCADE ON DELETE CASCADE
 );
