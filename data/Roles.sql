dc67ddade1a855a2ba92812eefd12c321de3ee08
Ernest Magriñá <ernest8081@gmail.com>
2023-08-19 21:49:15 +0200

    feat: :package: add Counntries and Populations

---
+++ data/Roles.sql
@@ -0,0 +1,5 @@
+INSERT INTO Roles (ID, Name, Description) 
+VALUES 
+(1, 'User', 'Role for regular users'),
+(2, 'Hotel Manager', 'Role for people managing hotels'),
+(3, 'Admin', 'Role for system administrators');
---
+++ data/Users.sql
@@ -0,0 +1,5 @@
+INSERT INTO Users (UUID, FirstName, LastName, Email, Password, Gender, ImageProfile, RoleId) 
+VALUES 
+('e731b1d2-85cf-4c01-95a6-47614cb5a587', 'John', 'Doe', 'johndoe', 'johndoe@example.com', 'password123', 'M', 1),
+('fd405768-0d1f-4a8f-9c46-5b1ff49db76a', 'Alice', 'Smith', 'alicesmith', 'alicesmith@example.com', 'securepass', 'F', 2),
+('c3d5584f-1b2e-4e6c-88c9-2dc2726e47c5', 'Michael', 'Johnson', 'michaelj', 'michaelj@example.com', 'mypass123', 'M', 3);
