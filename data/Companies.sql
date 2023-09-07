76f67dfc89f50edb2fbad05dc77df0603d075cbc
Ernest Magriñá <ernest8081@gmail.com>
2023-09-07 17:52:39 +0200

    feat: :package: add companies data

---
+++ data/Companies.sql
@@ -0,0 +1,14 @@
+INSERT INTO Companies (Id, Name, Url, LogoImg)
+VALUES 	(1, 'Agoda.com', 'https://www.agoda.com/es-es/', ''),
+		(2, 'Booking', 'https://www.booking.com/index.es.html', ''),
+        (3, 'eDreams', 'https://www.edreams.es/', ''),
+        (4, 'Expedia', 'https://www.expedia.com/es/', ''),
+        (5, 'Homestay.com', 'https://www.homestay.com/es', ''),
+        (6, 'Hotels', 'https://es.hoteles.com/', ''),
+        (7, 'Preferred Hotels & Resorts', 'https://preferredhotels.com/', ''),
+        (8, 'roomsXXL', 'https://www.roomsxxl.com/', ''),
+        (9, 'Rumbo', 'https://www.rumbo.es/', ''),
+        (10, 'Sembo', 'https://www.sembo.com/', ''),
+        (11, 'Stayforlong', 'https://es.stayforlong.com/', ''),
+        (12, 'Trip.com', 'https://es.trip.com/', ''),
+        (13, 'Vrbo', 'https://www.vrbo.com/es-es/', '');
